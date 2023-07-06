1bfc6882ad2ef3789afcf3fb4d2804baf14b3062
jakiedoan <jakiedoan@gmail.com>
2023-07-06 16:25:38 +0700

    Full version.

--- src/layout/character/character.js
+++ src/layout/character/character.js
@@ -1,33 +1,40 @@
 import './character.scss';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-const Character = ({windowDimensions}) =>{
-    const {t} = useTranslation();
+const Character = ({ windowDimensions }) => {
+  const { t } = useTranslation();
+  const video = process.env.PUBLIC_URL + '/video/character.mp4';
 
-    return(
-        <div className="character">
-            <span className="title-main"
-                  data-aos="fade-up">
-                {t("character.titleMain")}
-            </span>
-            {
-                windowDimensions.width > 767.5 ?
-                    <video data-aos="fade-up"
-                           data-aos-delay="600"
-                           autoPlay muted loop
-                           className="video hidden-mobile">
-                        <source src="" type="video/mp4"/>
-                    </video>
-                    :
-                    <video data-aos="fade-up"
-                           data-aos-delay="600"
-                           autoPlay muted loop
-                           className="video hidden-pc">
-                        <source src="" type="video/mp4"/>
-                    </video>
-            }
-        </div>
-    )
-}
+  return (
+    <div className='character'>
+      <span className='title-main' data-aos='fade-up'>
+        {t('character.titleMain')}
+      </span>
+      {windowDimensions.width > 767.5 ? (
+        <video
+          data-aos='fade-up'
+          data-aos-delay='600'
+          autoPlay
+          muted
+          loop
+          className='video hidden-mobile'
+        >
+          <source src={video} type='video/mp4' />
+        </video>
+      ) : (
+        <video
+          data-aos='fade-up'
+          data-aos-delay='600'
+          autoPlay
+          muted
+          loop
+          className='video hidden-pc'
+        >
+          <source src={video} type='video/mp4' />
+        </video>
+      )}
+    </div>
+  );
+};
 
 export default Character;
--- src/layout/mini-games/mini-games.js
+++ src/layout/mini-games/mini-games.js
@@ -1,107 +1,157 @@
 import './mini-games.scss';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-const MiniGames = ({windowDimensions}) =>{
-    const {t} = useTranslation();
-    const images = {
-        button : process.env.PUBLIC_URL + "/images/mini-games/button.png",
-    }
+const MiniGames = ({ windowDimensions }) => {
+  const { t } = useTranslation();
+  const images = {
+    button: process.env.PUBLIC_URL + '/images/mini-games/button.png',
+    hiddenObjects:
+      process.env.PUBLIC_URL + '/images/mini-games/hidden-objects.png',
+    jigsawPuzzle:
+      process.env.PUBLIC_URL + '/images/mini-games/jigsaw-puzzle.png',
+    quiz: process.env.PUBLIC_URL + '/images/mini-games/quiz.png',
+  };
 
-    return(
-        <div className="mini-games">
-            <span className="title-main"
-                  data-aos="fade-up">
-                {t("miniGames.titleMain")}
-            </span>
+  const video = {
+    miniGames: process.env.PUBLIC_URL + '/video/mini-games.mp4',
+  };
 
-            {
-                windowDimensions.width > 767.5 ?
-                    <>
-                        <video data-aos="fade-up"
-                               data-aos-delay="600"
-                               autoPlay muted loop
-                               className="video hidden-mobile">
-                            <source src=""
-                                    type="video/mp4"/>
-                        </video>
+  return (
+    <div className='mini-games'>
+      <span className='title-main' data-aos='fade-up'>
+        {t('miniGames.titleMain')}
+      </span>
 
-                        <div className="button-hidden-box">
-                            <div data-aos="flip-down"
-                                 className="button button-hidden hidden-mobile"
-                                 style={{backgroundImage:`url(${images.button}`}}>
-                                <span>{t("miniGames.buttonObject")}</span>
-                            </div>
-                        </div>
+      {windowDimensions.width > 767.5 ? (
+        <>
+          <video
+            data-aos='fade-up'
+            data-aos-delay='600'
+            autoPlay
+            muted
+            loop
+            className='video hidden-mobile'
+          >
+            <source src={video.miniGames} type='video/mp4' />
+          </video>
+          <div className='button-hidden-box'>
+            <div
+              data-aos='flip-down'
+              className='button button-hidden hidden-mobile'
+              style={{ backgroundImage: `url(${images.button}` }}
+            >
+              <span>{t('miniGames.buttonObject')}</span>
+            </div>
+          </div>
 
-                        <div data-aos="flip-down"
-                             className="button button-jigsaw hidden-mobile"
-                             style={{backgroundImage:`url(${images.button}`}}>
-                            <span>{t("miniGames.buttonPuzzle")}</span>
-                        </div>
+          <div
+            data-aos='flip-down'
+            className='button button-jigsaw hidden-mobile'
+            style={{ backgroundImage: `url(${images.button}` }}
+          >
+            <span>{t('miniGames.buttonPuzzle')}</span>
+          </div>
 
-                        <div data-aos="flip-down"
-                             className="button button-quiz hidden-mobile"
-                             style={{backgroundImage:`url(${images.button}`}}>
-                            <span>{t("miniGames.buttonQuiz")}</span>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <div className="mobile-box hidden-pc">
-                            <video autoPlay muted loop
-                                   className="video"
-                                   data-aos="fade-up"
-                                   data-aos-delay="800">
-                                <source src=""
-                                        type="video/mp4"/>
-                            </video>
-                            <div className="button-mobile">
-                                <div className="button"
-                                     data-aos="flip-down"
-                                     style={{backgroundImage:`url(${images.button}`}}>
-                                    <span>{t("miniGames.buttonObject")}</span>
-                                </div>
-                            </div>
-                        </div>
+          <div
+            data-aos='flip-down'
+            className='button button-quiz hidden-mobile'
+            style={{ backgroundImage: `url(${images.button}` }}
+          >
+            <span>{t('miniGames.buttonQuiz')}</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className='mobile-box hidden-pc'>
+            <img
+              className='video'
+              data-aos='fade-up'
+              data-aos-delay='800'
+              src={images.hiddenObjects}
+              alt=''
+            />
+            {/* <video
+              autoPlay
+              muted
+              loop
+              className='video'
+              data-aos='fade-up'
+              data-aos-delay='800'
+            >
+              <source src='' type='video/mp4' />
+            </video> */}
+            <div className='button-mobile'>
+              <div
+                className='button'
+                data-aos='flip-down'
+                style={{ backgroundImage: `url(${images.button}` }}
+              >
+                <span>{t('miniGames.buttonObject')}</span>
+              </div>
+            </div>
+          </div>
 
-                        <div className="mobile-box hidden-pc">
-                            <video autoPlay muted loop
-                                   className="video"
-                                   data-aos="fade-up"
-                                   data-aos-delay="800">
-                                <source src=""
-                                        type="video/mp4"/>
-                            </video>
-                            <div className="button-mobile">
-                                <div className="button"
-                                     data-aos="flip-down"
-                                     style={{backgroundImage:`url(${images.button}`}}>
-                                    <span>{t("miniGames.buttonPuzzle")}</span>
-                                </div>
-                            </div>
-                        </div>
+          <div className='mobile-box hidden-pc'>
+            {/* <video
+              autoPlay
+              muted
+              loop
+              className='video'
+              data-aos='fade-up'
+              data-aos-delay='800'
+            >
+              <source src='' type='video/mp4' />
+            </video> */}
+            <img
+              className='video'
+              data-aos='fade-up'
+              data-aos-delay='800'
+              src={images.jigsawPuzzle}
+              alt=''
+            />
+            <div className='button-mobile'>
+              <div
+                className='button'
+                data-aos='flip-down'
+                style={{ backgroundImage: `url(${images.button}` }}
+              >
+                <span>{t('miniGames.buttonPuzzle')}</span>
+              </div>
+            </div>
+          </div>
 
-                        <div className="mobile-box hidden-pc"
-                             data-aos="fade-up">
-                            <video autoPlay muted loop
-                                   className="video"
-                                   data-aos="fade-up"
-                                   data-aos-delay="800">
-                                <source src=""
-                                        type="video/mp4"/>
-                            </video>
-                            <div className="button-mobile">
-                                <div className="button"
-                                     data-aos="flip-down"
-                                     style={{backgroundImage:`url(${images.button}`}}>
-                                    <span>{t("miniGames.buttonQuiz")}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </>
-            }
-        </div>
-    )
-}
+          <div className='mobile-box hidden-pc' data-aos='fade-up'>
+            {/* <video
+              autoPlay
+              muted
+              loop
+              className='video'
+              data-aos='fade-up'
+              data-aos-delay='800'
+            >
+              <source src='' type='video/mp4' />
+            </video> */}
+            <img
+              className='video'
+              data-aos='fade-up'
+              data-aos-delay='800'
+              src={images.quiz}
+              alt=''
+            />
+            <div className='button-mobile'>
+              <div
+                className='button'
+                data-aos='flip-down'
+                style={{ backgroundImage: `url(${images.button}` }}
+              >
+                <span>{t('miniGames.buttonQuiz')}</span>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default MiniGames
+export default MiniGames;
